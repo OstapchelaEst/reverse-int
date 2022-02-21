cf81e82f9b1f81391401e9bfe05afb511865a3cc
OstapchelaEst <Ostapchela@gmail.com>
2022-02-22 01:53:35 +0300

    completed test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(numbers) {
+   let result = '';
+   numbers = numbers.toString().replace('-', '').split('').map(function (a) { result = a + result; })
+   return Number(result)
 }
